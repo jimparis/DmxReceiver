24aca648f59aa298dbe21e70919b37d72cefdfaf
Jim Paris <jim@jtan.com>
2014-05-01 18:45:17 -0400

    Import Ward's DmxReceiver code
    Source:
      http://forum.pjrc.com/threads/19662?p=24993#post24993

---
+++ DmxReceiver.cpp
@@ -0,0 +1,86 @@
+#include "DmxReceiver.h"
+#include "HardwareSerial.h"
+#define DMX_BUFFER_SIZE 513
+
+static volatile uint8_t dmxBuffer1[DMX_BUFFER_SIZE];
+static volatile uint8_t dmxBuffer2[DMX_BUFFER_SIZE];
+static volatile uint8_t *activeBuffer;
+static volatile uint8_t *inactiveBuffer;
+static volatile uint16_t dmxBufferIndex;
+static volatile unsigned int frameCount=0;
+static volatile bool newFrame=false;
+HardwareSerial Uart = HardwareSerial();
+
+void dmx_begin(void)
+{
+	//UART Initialization
+	Uart.begin(250000);
+	UART0_C3 = UART_C3_FEIE;  //Enable UART0 interrupt on frame error
+	NVIC_ENABLE_IRQ(IRQ_UART0_ERROR);  //Enable the frame error IRQ
+	activeBuffer=dmxBuffer1;
+	inactiveBuffer=dmxBuffer2;
+}
+
+void dmx_end(void)
+{
+	Uart.end();
+	NVIC_DISABLE_IRQ(IRQ_UART0_ERROR);  //Enable the frame error IRQ
+}
+
+unsigned int dmx_frameCount(void)
+{
+	return frameCount;
+}
+
+uint8_t dmx_getDimmer(uint16_t d)
+{
+	return inactiveBuffer[d];
+}
+
+int dmx_bufferService (void)
+{
+	__disable_irq(); //Prevents conflicts with the UART0 error ISR
+	int available=Uart.available();
+	int retval=available;
+	while (available--)
+	{
+		activeBuffer[dmxBufferIndex]=Uart.read();
+		if (dmxBufferIndex<(DMX_BUFFER_SIZE-1)) dmxBufferIndex++;
+	}
+	__enable_irq();
+	return retval;
+}
+
+bool dmx_newFrame(void)
+{
+	if (newFrame)
+	{
+		newFrame=false;
+		return true;
+	}
+	return false;
+}
+
+//UART0 will throw a frame error on the DMX break pulse.  That's our cue to switch buffers
+//and reset the index to zero
+void uart0_error_isr(void)
+{
+		if (UART0_S1 & UART_S1_FE)
+		{
+			(void) UART0_D;
+			frameCount++;
+			dmx_bufferService();
+			dmxBufferIndex=0;
+			if (activeBuffer==dmxBuffer1)
+			{
+				activeBuffer=dmxBuffer2;
+				inactiveBuffer=dmxBuffer1;
+			}
+			else
+			{
+				activeBuffer=dmxBuffer1;
+				inactiveBuffer=dmxBuffer2;
+			}
+			newFrame=true;
+		}
+}
---
+++ DmxReceiver.h
@@ -0,0 +1,29 @@
+#include <inttypes.h>
+
+#define UART_C3_FEIE		(uint8_t)0x02			//Framing Error Interrupt Enable
+#define UART_C5_RDMAS		(uint8_t)0x20			//Receiver Full DMA Select
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void dmx_begin(void);
+void dmx_end(void);
+int dmx_bufferService(void);
+uint8_t dmx_getDimmer(uint16_t);
+unsigned int dmx_frameCount(void);
+bool dmx_newFrame(void);
+#ifdef __cplusplus
+}
+#endif
+
+
+class DmxReceiver
+{
+public:
+	void begin(void) {dmx_begin();}
+	void end(void) {dmx_end();}
+	int bufferService(void) {return dmx_bufferService();}
+	uint8_t getDimmer(uint16_t d) {return dmx_getDimmer(d);}
+	unsigned int frameCount(void) {return dmx_frameCount();}
+	bool newFrame(void) {return dmx_newFrame();}
+};
