713a6ec3dd50bcfa995dc474465f0787983d002a
Jim Paris <jim@jtan.com>
2014-05-10 11:51:03 -0400

    Fix framing errors due to short "mark time between packets".
    Some of the data that was received before the BREAK may still have
    been hidden inside the hardware RX FIFO.  Reduce the FIFO waterlevel
    mark, so that the existing Teensy code pulls those bytes out right away.

--- DmxReceiver.cpp
+++ DmxReceiver.cpp
@@ -13,18 +13,30 @@ HardwareSerial Uart = HardwareSerial();
 
 void dmx_begin(void)
 {
-	//UART Initialization
+	// UART Initialization
 	Uart.begin(250000);
-	UART0_C3 = UART_C3_FEIE;  //Enable UART0 interrupt on frame error
-	NVIC_ENABLE_IRQ(IRQ_UART0_ERROR);  //Enable the frame error IRQ
-	activeBuffer=dmxBuffer1;
-	inactiveBuffer=dmxBuffer2;
+
+        // Fire UART0 receive interrupt immediately after each byte received
+	UART0_RWFIFO = 1;
+
+        // Set error IRQ priority lower than that of the status IRQ,
+        // so that the status IRQ receives any leftover bytes before
+        // we detect and trigger a new frame.
+	NVIC_SET_PRIORITY(IRQ_UART0_ERROR,
+                          NVIC_GET_PRIORITY(IRQ_UART0_STATUS) + 1);
+
+        // Enable UART0 interrupt on frame error and enable IRQ
+	UART0_C3 |= UART_C3_FEIE;
+	NVIC_ENABLE_IRQ(IRQ_UART0_ERROR);
+
+	activeBuffer = dmxBuffer1;
+	inactiveBuffer = dmxBuffer2;
 }
 
 void dmx_end(void)
 {
 	Uart.end();
-	NVIC_DISABLE_IRQ(IRQ_UART0_ERROR);  //Enable the frame error IRQ
+	NVIC_DISABLE_IRQ(IRQ_UART0_ERROR);
 }
 
 void dmx_clear(void)
@@ -69,26 +81,33 @@ bool dmx_newFrame(void)
 	return false;
 }
 
-//UART0 will throw a frame error on the DMX break pulse.  That's our cue to switch buffers
-//and reset the index to zero
+// UART0 will throw a frame error on the DMX break pulse.  That's our
+// cue to switch buffers and reset the index to zero
 void uart0_error_isr(void)
 {
-		if (UART0_S1 & UART_S1_FE)
-		{
-			(void) UART0_D;
-			frameCount++;
-			dmx_bufferService();
-			dmxBufferIndex=0;
-			if (activeBuffer==dmxBuffer1)
-			{
-				activeBuffer=dmxBuffer2;
-				inactiveBuffer=dmxBuffer1;
-			}
-			else
-			{
-				activeBuffer=dmxBuffer1;
-				inactiveBuffer=dmxBuffer2;
-			}
-			newFrame=true;
-		}
+        // On break, uart0_status_isr() will probably have already
+        // fired and read the data buffer, clearing the framing error.
+        // If for some reason it hasn't, make sure we consume the 0x00
+        // byte that was received.
+        if (UART0_S1 & UART_S1_FE)
+                (void) UART0_D;
+
+        // Ensure we've processed all the data that may still be sitting
+        // in software buffers.
+        dmx_bufferService();
+
+        // Update frame count and swap buffers
+        frameCount++;
+        dmxBufferIndex = 0;
+        if (activeBuffer == dmxBuffer1)
+        {
+                activeBuffer = dmxBuffer2;
+                inactiveBuffer = dmxBuffer1;
+        }
+        else
+        {
+                activeBuffer = dmxBuffer1;
+                inactiveBuffer = dmxBuffer2;
+        }
+        newFrame=true;
 }
